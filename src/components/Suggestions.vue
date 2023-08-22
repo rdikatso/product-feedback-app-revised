57e167ac58c4efcb921686698f5b513eb0997f03
Dikatso Ramatsela <dikatsoramatsela@Dikatsos-MacBook-Pro.local>
2023-08-21 23:54:24 -0400

    [Dikatso] styling changes

--- src/components/Suggestions.vue
+++ src/components/Suggestions.vue
@@ -21,7 +21,20 @@
                 <p>Roadmap</p>
                 <a href="/">View</a>
             </div>
-            <div class="roadmap-progress d-flex justify-content-between">
+            <div class="roadmap-items d-flex row" v-for="(value, key) in status" :key="key">
+                <div class="col-md-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8" fill="none">
+                        <circle cx="4" cy="4" r="4" fill="#F49F85" 
+                        :class="[key.toLowerCase() == 'suggestion'  ? 'suggestion-color' : '', 
+                        key.toLowerCase() == 'planned'  ? 'planned-color' : '',
+                        key.toLowerCase() == 'in-progress'  ? 'in-progress-color' : '',
+                        key.toLowerCase() == 'live'  ? 'live-color' : '',]"/>
+                    </svg>
+                </div>
+                <p class="status col-md-8">{{key}}</p>
+                <p class="status-count col-md-2">{{value}}</p>
+            </div>
+            <!-- <div class="roadmap-progress d-flex justify-content-between">
                 <p>&#x2022; Planned</p>
                 <p>2</p>
             </div>
@@ -32,7 +45,7 @@
             <div class="roadmap-progress d-flex justify-content-between">
                 <p>&#x2022; Live</p>
                 <p>1</p>
-            </div>
+            </div> -->
         </div>
     </div>
     <div class="col-md-9">
@@ -64,7 +77,7 @@
             </div>
         </div>
         <div class="row">
-            <div class="suggestion-container d-flex justify-content-center align-items-center" v-for="request in productRequests" :key="request.index">
+            <div class="suggestion-container d-flex justify-content-center align-items-center" v-for="(request, index) in productRequests" :key="index">
                 <div class="upvotes-container col-md-1">
                     <i class="fa-solid fa-chevron-up"></i>
                     <p class="upvotes">{{request.upvotes}}</p>
@@ -82,7 +95,7 @@
                 </div>
             </div>
         </div>
-        <div> {{productRequests}}</div>
+        <!-- <div> {{productRequests}}</div> -->
     </div>
   </div>
 </template>
@@ -122,54 +135,95 @@ export default {
             uniqueCategories.unshift('All');
             console.log("categories after filtering", uniqueCategories)
             return uniqueCategories
+        },
+        status(){
+
+            //The solution below works. But its an array of arrays which I dont know how to iterate over yet.
+         
+            // const statusArray = this.productRequests.map((item) => {
+            //     return item.status
+            // })
+            // console.log(statusArray)
+            // let uniqueStatus = [...new Set(statusArray)];
+            // console.log("UNIQUE STATUS", uniqueStatus)
+            // const elementCounts = uniqueStatus.map(value => [value, statusArray.filter(str => str === value).length]);
+            // console.log(elementCounts);
+            // return elementCounts
+
+            const statusArray = this.productRequests.map((item) => {
+                return item.status
+            })
+            const statusCounts = {};
+            statusArray.forEach(status => {
+                statusCounts[status] = (statusCounts[status] || 0) + 1;
+            })
+            console.log("STATUS COUNTS", statusCounts)
+
+            return statusCounts
         }
     }
 }
 </script>
 
 <style lang="scss">
+body {
+    background: #F7F8FD;
+}
 .logo-container {
-    border: 1px solid #000;
-    border-radius: 5px;
-    min-height: 125px;
+    background-image : url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" width="255" height="137" viewBox="0 0 255 137" fill="none"><rect width="255" height="137" rx="10" fill="url(%23paint0_radial_0_2333)"/><mask id="mask0_0_2333" style="mask-type:luminance" maskUnits="userSpaceOnUse" x="0" y="0" width="255" height="137"><rect width="255" height="137" rx="10" fill="white"/></mask><g mask="url(%23mask0_0_2333)"><g opacity="0.765433" filter="url(%23filter0_f_0_2333)"><circle cx="-0.5" cy="-40.5" r="96" fill="%237AD8FB"/></g><g filter="url(%23filter1_f_0_2333)"><circle cx="304.5" cy="137.5" r="96" fill="%23FBB57A"/></g></g><defs><filter id="filter0_f_0_2333" x="-186.203" y="-226.203" width="371.407" height="371.407" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/><feGaussianBlur stdDeviation="44.8517" result="effect1_foregroundBlur_0_2333"/></filter><filter id="filter1_f_0_2333" x="118.797" y="-48.2033" width="371.407" height="371.407" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feBlend mode="normal" in="SourceGraphic" in2="BackgroundImageFix" result="shape"/><feGaussianBlur stdDeviation="44.8517" result="effect1_foregroundBlur_0_2333"/></filter><radialGradient id="paint0_radial_0_2333" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(264.945 -14.2296) rotate(146.823) scale(322.666 301.288)"><stop stop-color="%23E84D70"/><stop offset="0.530886" stop-color="%23A337F6"/><stop offset="1" stop-color="%2328A7ED"/></radialGradient></defs></svg>');
+    background-repeat:no-repeat;
+    background-size: cover;
+    border-radius: 0.625rem;
+    min-height: 8.5625rem;
     padding: 15px;
     .logo {
-        font-size: 18px;
-        font-weight: bold;
+        font-size: 1.25rem;
+        font-style: normal;
+        font-weight: 700;
+        line-height: normal;
+        letter-spacing: -0.01563rem;
+        color: #fff;
+    }
+    .tagline {
+        color: #FFF;
+        font-size: 0.9375rem;
+        font-style: normal;
+        font-weight: 500;
+        line-height: normal;
+        opacity: 0.75;
     }
 }
 .suggestions-categories {
-    border: 1px solid #000;
-    border-radius: 5px;
-    min-height: 10.375rem;
+    height: 10.375rem;
     padding: 15px; 
     margin-top: 20px;
     display: flex;
+    align-items: center;
+    justify-content: space-between;
     flex-wrap: wrap;
+    border-radius: 0.625rem;
+    background: #FFF;  
+    flex-shrink: 0; 
 
     a {
         text-decoration: none;
-        border: 1px solid #000;
         border-radius: 5px;
-        margin-right: 25px;
+        // margin-right: 25px;
         height: 1.875rem;
-        background-color: red;
+        border-radius: 0.625rem;
+        background: #F2F4FF;
+        display: flex;
+
+        font-size: 0.8125rem;
+        font-style: normal;
+        font-weight: 600;
+        line-height: normal;
 
         span {
-            padding: 10px;
+            padding: 0.31rem 1rem 0.38rem 1rem;
             font-size: 0.8125rem;
         }
     }
-}
-.roadmap-container {
-    margin-top: 20px;
-    padding: 15px;
-    border: 1px solid #000;
-    border-radius: 5px;
-    min-height: 10.375rem;
-    // .roadmap-heading{
-        
-    // }
 }
 .add-feedback-container{
     background-color: #373F68;
@@ -198,5 +252,62 @@ export default {
         padding-top: 0.72rem;
     }
 }
+.roadmap-container {
+    margin-top: 20px;
+    padding: 1.5rem;
+    border-radius: 0.625rem;
+    min-height: 10.375rem;
+    background: #FFFFFF;
+    .roadmap-heading {
+        margin-bottom: 1.5rem;
+        font-style: normal;
+        line-height: normal;
+        p {
+            color: #3A4374;
+            font-size: 1.125rem;
+            font-weight: 700;
+            letter-spacing: -0.01563rem;
+        }
+        a{
+            color: #4661E6;
+            font-size: 0.8125rem;
+            font-weight: 600;
+            text-decoration-line: underline;
+        }
+    }
+
+    .roadmap-items {
+        color: #647196;
+        font-size: 1rem;
+        font-weight: 400;
+        .status {
+            text-transform: capitalize;
+            color: #647196;
+            font-size: 1rem;
+            font-weight: 400;
+            text-align: left;
+        }
+        .status-count {
+            color: #647196;
+            text-align: right;
+            font-size: 1rem;
+            font-weight: 700;
+        }
+        .suggestion-color{
+            fill: #5FA052;
+        }
+        .planned-color{
+            fill: #F49F85;
+        }
+        .in-progress-color{
+            fill: #AD1FEA;
+        }
+        .live-color{
+           fill: #62BCFA;
+        }
+    }
+    
+}
+
 
 </style>
